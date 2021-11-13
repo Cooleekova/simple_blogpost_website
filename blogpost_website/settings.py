36105da44bdf28d20e4a201dd59e9a9a301e0b53
Atya <nushnushka@gmail.com>
2021-11-13 20:18:46 +0300

    django_heroku added

--- blogpost_website/settings.py
+++ blogpost_website/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secret_key
+# SECRET_KEY = secret_key
+SECRET_KEY = 'kjzdghzdrgxd36vth8fhfjhgkxdjhcgu56gckjhgzlkdghc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
