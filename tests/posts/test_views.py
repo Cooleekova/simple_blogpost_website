d33863419c3bfff98ee132154a0182bb94f5af89
Atya <nushnushka@gmail.com>
2021-11-13 20:28:41 +0300

    django_heroku added

--- tests/posts/test_views.py
+++ tests/posts/test_views.py
@@ -1,16 +1,23 @@
 import pytest
-from django.urls import reverse
-from rest_framework.status import HTTP_302_FOUND
+# from django.urls import reverse
+# from rest_framework.status import HTTP_302_FOUND
+from django.contrib.auth.models import User
 
 
-# проверка получения списка постов
 @pytest.mark.django_db
-def test_get_posts(client, post_factory):
+def test_user_create():
+    User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+    assert User.objects.count() == 1
 
-    # создаём десять постов
-    post_factory(_quantity=10)
-    url = reverse("posts:posts")
-    # совершаем запрос GET к API по URL
-    resp = client.get(url)
-    assert resp.status_code == HTTP_302_FOUND
 
+#
+# # проверка редиректа неавторизованного пользователя
+# # вместо получения списка постов на login page
+# @pytest.mark.django_db
+# def test_get_posts(auth_client, post_factory):
+#     # создаём десять постов
+#     post_factory(_quantity=10)
+#     url = reverse("posts:posts")
+#     # совершаем запрос GET к API по URL
+#     resp = auth_client.get(url)
+#     assert resp.status_code == HTTP_302_FOUND
