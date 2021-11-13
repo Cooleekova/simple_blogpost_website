03096d941967b895fa191e3ab415b670d91a3c7c
Atya <nushnushka@gmail.com>
2021-11-13 20:01:11 +0300

    tests added

--- blogpost_website/settings.py
+++ blogpost_website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = secret_key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', ]
+ALLOWED_HOSTS = ['127.0.0.1', 'https://atya-blog-for-friends.herokuapp.com', ]
 
 
 # Application definition
